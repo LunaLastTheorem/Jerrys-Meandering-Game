0560624a9f1b41d5b0c00fde3834bb88117a4fb8
Meghan Buchanan <meghan.buchanan@hotmail.com>
2025-10-14 14:46:26 -0700

    we have a grid

--- game.py
+++ game.py
@@ -10,8 +10,13 @@ RED = (255, 0, 0)
 pygame.init()
 
 # Set up the game window
-size = (700, 500)
+size = (255, 255)
 screen = pygame.display.set_mode(size)
+
+# Grid settings
+width = 20
+height = 20
+margin = 5 # this is margin between each box and edges of screen
 
 pygame.display.set_caption("Super Cool and Awesome Game")
 
@@ -31,7 +36,20 @@ while not done:
 
     # We clear the screen to white. DO NOT put other drawing commands
     # above this, or they WILL be erased with this command.
-    # If you want a background 
+    # If you want a background image, replace this clear with blit'ing
+    # background image.
+    screen.fill(BLACK)
+
+    # --- Drawing code should go here
+    for column in range(10):
+        for row in range(10):
+            pygame.draw.rect(screen, WHITE, [(margin + width) * column + margin, (margin + height) * row + margin, width, height], 0)
+
+    # --- Go ahead and update the screen with what we've drawn
+    pygame.display.flip()
+
+    # --- Limit 60 frames per second
+    clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
