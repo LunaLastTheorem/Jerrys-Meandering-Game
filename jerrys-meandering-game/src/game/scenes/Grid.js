760a0e2b717656f592f9d298401a18763c2f9733
Meghan Buchanan <meghan.buchanan@hotmail.com>
2025-11-04 15:57:08 -0800

    making the background of circle changing colors when clicked

--- jerrys-meandering-game/src/game/scenes/Grid.js
+++ jerrys-meandering-game/src/game/scenes/Grid.js
@@ -7,11 +7,15 @@ export class Grid extends Scene {
         super("Grid");
     }
 
-    preload() { }
+    preload() {
+        this.white = 0xFFFFFF;
+        this.lightblue = 0x8091F5;
+        this.lightred = 0xF48A8D;
+        this.red = 0xE9141D;
+        this.blue = 0x0015BC;
+    }
 
     create() {
-        const red = 0xE9141D;
-        const blue = 0x0015BC;
 
         const rows = myData.rows;
         const cols = myData.cols;
@@ -20,38 +24,48 @@ export class Grid extends Scene {
         const offsetX = (this.scale.width - cols * cellSize) / 2;
         const offsetY = (this.scale.height - rows * cellSize) / 2;
 
-        this.grid = [];
+        this.circles = [];
+        this.cells = [];
 
         for (let row = 0; row < rows; row++) {
-            this.grid[row] = [];
+            this.circles[row] = [];
+            this.cells[row] = [];
+
             for (let col = 0; col < cols; col++) {
                 const x = offsetX + col * cellSize + cellSize / 2;
                 const y = offsetY + row * cellSize + cellSize / 2;
 
-                const colors = [red, blue];
+                const colors = [this.red, this.blue];
                 const currColor = myData.grid[row][col]
                 const color = colors[currColor === "r" ? 0 : 1];
 
-                const cell = this.add.circle(x, y, cellSize / 2, color);
-                cell.setStrokeStyle(3, 0xffffff);
-                cell.currentStroke = 0xffffff;
+                const cell = this.add.rectangle(x, y, cellSize, cellSize, this.white);
+                const circle = this.add.circle(x, y, cellSize / 2 - 2, color);
+
+                cell.fillColor = this.white;
+                circle.fillColor = color;
+
                 cell.setInteractive();
+                circle.setInteractive();
 
                 cell.row = row;
                 cell.col = col;
+                circle.row = row;
+                circle.col = col;
 
-                cell.on('pointerover', () => {
-                    cell.setAlpha(0.5);
+                circle.on('pointerover', () => {
+                    circle.setAlpha(0.5);
                 });
-                cell.on('pointerout', () => {
-                    cell.setAlpha(1);
-                });
-
-                cell.on("pointerdown", () => {
-                    this.handleClickCell(cell);
+                circle.on('pointerout', () => {
+                    circle.setAlpha(1);
                 });
 
-                this.grid[row][col] = cell;
+                circle.on("pointerdown", () => {
+                    this.handleClickCell(cell, circle);
+                });
+
+                this.circles[row][col] = circle;
+                this.cells[row][col] = cell;
 
             }
         }
@@ -59,13 +73,24 @@ export class Grid extends Scene {
         EventBus.emit('current-scene-ready', this);
     }
 
-    handleClickCell(cell) {
+    handleClickCell(cell, circle) {
         console.log(`Clicked cell at row ${cell.row}, col ${cell.col}`);
 
-        const isWhite = cell.currentStroke === 0xffffff;
-        const newStroke = isWhite ? 0x000000 : 0xffffff;
-        cell.currentStroke = newStroke;
-        cell.setStrokeStyle(3, newStroke);
+        // const isWhite = cell.currentStroke === 0xffffff;
+        // const newStroke = isWhite ? 0x000000 : 0xffffff;
+        // cell.currentStroke = newStroke;
+        // cell.setStrokeStyle(3, newStroke);
+
+        if (cell.fillColor === this.lightblue || cell.fillColor === this.lightred) {
+            cell.setFillStyle(this.white);
+            cell.fillColor = this.white;
+        } else if (circle.fillColor === this.red) {
+            cell.setFillStyle(this.lightred);
+            cell.fillColor = this.lightred;
+        } else {
+            cell.setFillStyle(this.lightblue);
+            cell.fillColor = this.lightblue;
+        }
 
         EventBus.emit('grid-cell-clicked', { row: cell.row, col: cell.col });
     }
