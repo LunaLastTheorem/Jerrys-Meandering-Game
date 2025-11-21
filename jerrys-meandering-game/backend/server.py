2cc9f51449ea6a560e173aa1db4fc744d88a3104
dluna <dluna777@outlook.com>
2025-11-20 20:19:27 -0800

    ability to make backend return static website

--- jerrys-meandering-game/backend/server.py
+++ jerrys-meandering-game/backend/server.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pathlib import Path
 import os, certifi
 from dotenv import load_dotenv
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder='../dist', template_folder='../dist')
 CORS(app, origins=["http://localhost:8080"])
 
 load_dotenv()
@@ -22,6 +23,14 @@ def get_puzzle(index):
         return jsonify(puzzle)
     else: 
         return jsonify({"error": "Puzzle not found :("}), 404
+
+@app.route("/")
+def serve():
+    try:
+        return render_template("index.html")
+    except Exception as e:
+        print(f"not running static site {e}")
+    return "insert game here"
 
 if __name__ == "__main__":
     app.run(debug=True)
