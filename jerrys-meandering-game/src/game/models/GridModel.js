f757e09eac783e5e79a89227a7c279e5d15e4e41
Meghan Buchanan <meghan.buchanan@hotmail.com>
2026-02-18 14:14:55 -0800

    Adding docs to GridModel.js

--- jerrys-meandering-game/src/game/models/GridModel.js
+++ jerrys-meandering-game/src/game/models/GridModel.js
@@ -1,9 +1,24 @@
 
 /**
- * This class represents the Grid state.
+ * This class represents the Grid state. It has the following fields:
+ * rows - the number of rows in the puzzle
+ * cols - the number of columns in the puzzle
+ * districtSize - the size that the district should be
+ * cells - an object that contains:
+ *         r (row index), 
+ *         c (column index), 
+ *         isBlue (true if circle is blue, false if red), 
+ *         locked (true if cell is in district, 
+ *         false if not), 
+ *         and active (true if cell is selected, false if not)
  */
 export class GridModel {
 
+    /**
+     * The constructor initializes all fields of the class based on the puzzle object.
+     * 
+     * @param {object} puzzle 
+     */
     constructor(puzzle) {
         this.rows = puzzle.rows;
         this.cols = puzzle.cols;
@@ -25,6 +40,13 @@ export class GridModel {
         }
     }
 
+    /**
+     * This method gets the cell object at the specified row and column.
+     * 
+     * @param {integer} r the row index of the desired cell
+     * @param {integer} c the column index of the desired cell
+     * @returns the cell object
+     */
     getCell(r, c) {
         return this.cells[r][c];
     }
